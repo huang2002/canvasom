ac20b1fd204547df270c10390bb26c91eae7739a
hhh <1340545944@qq.com>
2020-02-06 10:53:07 +0800

    fix missing computing hook

--- src/nodes/Group.ts
+++ src/nodes/Group.ts
@@ -5,6 +5,14 @@ export class Group extends Node {
 
     penetrable = true;
 
+    protected _compute() {
+        const { childNodes } = this;
+        childNodes.forEach(childNode => {
+            childNode.compute();
+        })
+        this.bounds.contain(childNodes);
+    }
+
     protected _render(context: CanvasRenderingContext2D) {
         Utils.renderNodes(this.childNodes, context);
     }
